42be621e1e8a3a055d09e09c30ce989405f6d3a1
MBANS8A1 <seanmbng@aol.com>
2025-07-20 23:15:28 +0100

    In the Util.kt file, I have added two formatDate() functions. The first takes a date object and returns it as a String in "dd/MM/yyyy" (e.g. 09/10/2024). The next one takes a Calender instance and by using the day of the week range from Sunday-Saturday (1-7) , which is chosen randomly, a random date over the next week is returned as a String.

--- app/src/main/java/com/example/income_and_expenses_application/util/Util.kt
+++ app/src/main/java/com/example/income_and_expenses_application/util/Util.kt
@@ -1,6 +1,11 @@
 package com.example.income_and_expenses_application.util
 
 import androidx.compose.ui.graphics.Color
+import java.text.SimpleDateFormat
+import java.time.DayOfWeek
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 object Util {
     val incomeColour = listOf(
@@ -17,4 +22,24 @@ object Util {
         Color(0xFFFFAC12),
         Color(0xFFFF6951),
     )
+}
+
+//returns the date object a String in dd-MM-yyyy format
+ fun formatDate(
+    date: Date
+):String{
+   return SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+       .format(date)
+}
+private val date = Calendar.getInstance()
+
+//returns a Calendar instance as a random date range from now over the following week
+private fun formatDate(
+   date: Calendar
+):String{
+    val cal = date
+    val dayRange = 1..7
+    cal.set(Calendar.DAY_OF_WEEK,dayRange.random())
+    return SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        .format(cal.time)
 }
