489d56f8c8a36436d5923af109a5355685aaa287
MBANS8A1 <seanmbng@aol.com>
2025-07-20 18:06:28 +0100

    In the Util object, I have added the specific colour themes I want to use for income and expense, represented by incomeColour and expenseColour, respectively.

--- app/src/main/java/com/example/income_and_expenses_application/util/Util.kt
+++ app/src/main/java/com/example/income_and_expenses_application/util/Util.kt
@@ -1,4 +1,20 @@
 package com.example.income_and_expenses_application.util
 
+import androidx.compose.ui.graphics.Color
+
 object Util {
+    val incomeColour = listOf(
+        Color(0xFF37EFBA),
+        Color(0xFF04B97F),
+        Color(0xFF005D57),
+        Color(0xFF29B82F),
+        Color(0xFF008605)
+    )
+    val expenseColour = listOf(
+        Color(0xFFFFD7D0),
+        Color(0xFFFFDC78),
+        Color(0xFFFFAC12),
+        Color(0xFFFFAC12),
+        Color(0xFFFF6951),
+    )
 }
