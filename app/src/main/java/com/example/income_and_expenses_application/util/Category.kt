d77f97ab2d3763d1e5e304733dcd37be0f615c36
MBANS8A1 <seanmbng@aol.com>
2025-07-20 16:05:53 +0100

    In the res/drawable folder, i have added the necessary custom icons, in order to add extra interactivity to my income and expenses application. Also, I have added the dependencies to the Category enum in the Category.kt file; the individual Category object constants will make use of the drawables.

--- app/src/main/java/com/example/income_and_expenses_application/util/Category.kt
+++ app/src/main/java/com/example/income_and_expenses_application/util/Category.kt
@@ -1,5 +1,13 @@
 package com.example.income_and_expenses_application.util
 
-enum class Category {
+import androidx.compose.ui.graphics.Color
+
+enum class Category(
+    val title: String,
+    val iconRes: Int,
+    val bgRes: Int,
+    val colourRes: Color = Color.White
+
+) {
 
 }
