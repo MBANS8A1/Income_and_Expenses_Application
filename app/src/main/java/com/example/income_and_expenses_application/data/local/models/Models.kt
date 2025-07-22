6189eec0fb031158f9e023a6d453509a68dd7b53
MBANS8A1 <seanmbng@aol.com>
2025-07-22 14:48:42 +0100

    In the Models.kt file (within the local directory), I created my first entity called Expense with the properties I need( id, title,description, expenseAmount, category, entryDate and date). I used the @ColumnInfo annotations to associate specific properties (expense_id,expenseAmount and entry_date) with custom fields in my database.

--- app/src/main/java/com/example/income_and_expenses_application/data/local/models/Models.kt
+++ app/src/main/java/com/example/income_and_expenses_application/data/local/models/Models.kt
@@ -1,2 +1,21 @@
 package com.example.income_and_expenses_application.data.local.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "expense_table")
+data class Expense(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="expense_id")
+    val id: Int = 0,
+    val title: String,
+    val description:String,
+    @ColumnInfo(name="expense_amount")
+    val expenseAmount:Double,
+    val category:String,
+    @ColumnInfo(name="entry_date")
+    val entryDate:String,
+    val date: Date
+)
