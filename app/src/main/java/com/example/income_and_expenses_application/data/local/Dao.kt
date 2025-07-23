77a928877b29fe636a9eb2b1c8db125dda5f4c88
MBANS8A1 <seanmbng@aol.com>
2025-07-23 08:31:25 +0100

    I created my first data access object called ExpenseDao, which is an interface with method signatures (mostly suspend methods) for deleting an expense or expenses, updating an expense and getting a single expense or more than one expense. For methods annotated with @Update and @Insert, I have decided to use the REPLACE conflict strategy when there are conflict issues (e.g. records being added with the same id). I have used Flow as a return type with methods where I expect a stream of values to be returned asynchronously.

--- app/src/main/java/com/example/income_and_expenses_application/data/local/Dao.kt
+++ app/src/main/java/com/example/income_and_expenses_application/data/local/Dao.kt
@@ -1,2 +1,31 @@
 package com.example.income_and_expenses_application.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.income_and_expenses_application.data.local.models.Expense
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ExpenseDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExpense(expense:Expense)
+
+    @Query("DELETE FROM expense_table")
+    suspend fun deleteAllExpenses()
+
+    @Query("DELETE FROM expense_table WHERE expense_id=:id")
+    suspend fun deleteExpense(id:Int):Int
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateExpense(expense: Expense)
+
+    @Query("SELECT * FROM expense_table")
+    fun getAllExpenses(): Flow<List<Expense>>
+
+    @Query("SELECT * FROM expense_table WHERE expense_id=:id")
+    fun getExpenseById(id:Int):Flow<Expense>
+
+}
