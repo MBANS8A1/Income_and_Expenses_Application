298ec80af7e5cc86e7e1d0bd2e3beee3e18f04ff
MBANS8A1 <seanmbng@aol.com>
2025-07-23 09:20:29 +0100

    I created the second data access object called IncomeDao below the first Dao (ExpenseDao), which uses the income_id and income_table.

--- app/src/main/java/com/example/income_and_expenses_application/data/local/Dao.kt
+++ app/src/main/java/com/example/income_and_expenses_application/data/local/Dao.kt
@@ -6,6 +6,7 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.example.income_and_expenses_application.data.local.models.Expense
+import com.example.income_and_expenses_application.data.local.models.Income
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -27,5 +28,25 @@ interface ExpenseDao{
 
     @Query("SELECT * FROM expense_table WHERE expense_id=:id")
     fun getExpenseById(id:Int):Flow<Expense>
+}
 
+@Dao
+interface IncomeDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertIncome(income: Income)
+
+    @Query("DELETE FROM income_table")
+    suspend fun deleteAllIncomes()
+
+    @Query("DELETE FROM income_table WHERE income_id=:id")
+    suspend fun deleteIncome(id:Int):Int
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateIncome(income: Income)
+
+    @Query("SELECT * FROM income_table")
+    fun getAllIncomes(): Flow<List<Income>>
+
+    @Query("SELECT * FROM income_table WHERE income_id=:id")
+    fun getIncomeById(id:Int):Flow<Income>
 }
