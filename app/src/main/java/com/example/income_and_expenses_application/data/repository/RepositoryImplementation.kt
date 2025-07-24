3ac2e84723d8e39843d1cee5c598099183710a29
MBANS8A1 <seanmbng@aol.com>
2025-07-24 15:27:45 +0100

    In the RepositoryImplementation.kt file, I have added the implementation of the Repository method definitions. In relation to income and expense, I have used the incomeDao and ExpenseDao for the updating, getting, inserting and deleting of expense(s) and income(s). I have made use of context switching of withContext() to switch threads, in order to create a suspend block where the database operations with the data access objects can occur.

--- app/src/main/java/com/example/income_and_expenses_application/data/repository/RepositoryImplementation.kt
+++ app/src/main/java/com/example/income_and_expenses_application/data/repository/RepositoryImplementation.kt
@@ -6,6 +6,7 @@ import com.example.income_and_expenses_application.data.local.models.Expense
 import com.example.income_and_expenses_application.data.local.models.Income
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RepositoryImplementation @Inject constructor(
@@ -14,40 +15,43 @@ class RepositoryImplementation @Inject constructor(
     private val dispatcher: CoroutineDispatcher
 ):Repository {
     override val income: Flow<List<Income>>
-        get() = TODO("Not yet implemented")
+        get() = incomeDao.getAllIncomes()
     override val expense: Flow<List<Expense>>
-        get() = TODO("Not yet implemented")
+        get() = expenseDao.getAllExpenses()
 
-    override suspend fun insertIncome(income: Income) {
-        TODO("Not yet implemented")
+    override suspend fun insertIncome(income: Income) =
+        withContext(dispatcher){
+            incomeDao.insertIncome(income)
     }
 
-    override suspend fun insertExpense(expense: Expense) {
-        TODO("Not yet implemented")
+    override suspend fun insertExpense(expense: Expense)=
+    withContext(dispatcher){
+        expenseDao.insertExpense(expense)
     }
 
-    override fun getIncomeById(id: Int): Flow<Income> {
-        TODO("Not yet implemented")
-    }
+    override fun getIncomeById(id: Int): Flow<Income> =
+        incomeDao.getIncomeById(id)
 
-    override fun getExpenseById(id: Int): Flow<Expense> {
-        TODO("Not yet implemented")
-    }
+    override fun getExpenseById(id: Int): Flow<Expense> =
+        expenseDao.getExpenseById(id)
 
-    override suspend fun updateIncome(income: Income) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun updateIncome(income: Income) =
+        withContext(dispatcher){
+            incomeDao.updateIncome(income)
+        }
+    override suspend fun updateExpense(expense: Expense) =
+        withContext(dispatcher){
+            expenseDao.updateExpense(expense)
+        }
 
-    override suspend fun updateExpense(expense: Expense) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteIncome(id: Int): Int  =
+        withContext(dispatcher){
+            incomeDao.deleteIncome(id)
+        }
 
-    override suspend fun deleteIncome(id: Int): Int {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteExpense(id: Int): Int {
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteExpense(id: Int): Int  =
+        withContext(dispatcher){
+            expenseDao.deleteExpense(id)
+        }
 }
 
