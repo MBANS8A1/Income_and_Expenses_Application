e043bd1b884a65cd5c1de8c1c4a89a8e5e81999f
MBANS8A1 <seanmbng@aol.com>
2025-07-24 12:23:32 +0100

    In the RepositoryImplementation class I added the Repository methods, which need to be implemented. In addition, I used the @Inject annotation with a constructor to add dependencies, which will be used with the dependency injection module. The dependencies I added are incomeDao, expenseDao and a CoroutineDispatcher, which will help with switching to a different thread from a shared pool, when I am using the suspend functions.

--- app/src/main/java/com/example/income_and_expenses_application/data/repository/RepositoryImplementation.kt
+++ app/src/main/java/com/example/income_and_expenses_application/data/repository/RepositoryImplementation.kt
@@ -1,4 +1,53 @@
 package com.example.income_and_expenses_application.data.repository
 
-class RepositoryImplementation {
+import com.example.income_and_expenses_application.data.local.ExpenseDao
+import com.example.income_and_expenses_application.data.local.IncomeDao
+import com.example.income_and_expenses_application.data.local.models.Expense
+import com.example.income_and_expenses_application.data.local.models.Income
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class RepositoryImplementation @Inject constructor(
+    private val incomeDao: IncomeDao,
+    private val expenseDao: ExpenseDao,
+    private val dispatcher: CoroutineDispatcher
+):Repository {
+    override val income: Flow<List<Income>>
+        get() = TODO("Not yet implemented")
+    override val expense: Flow<List<Expense>>
+        get() = TODO("Not yet implemented")
+
+    override suspend fun insertIncome(income: Income) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun insertExpense(expense: Expense) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getIncomeById(id: Int): Flow<Income> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExpenseById(id: Int): Flow<Expense> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateIncome(income: Income) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun updateExpense(expense: Expense) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteIncome(id: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteExpense(id: Int): Int {
+        TODO("Not yet implemented")
+    }
 }
+
