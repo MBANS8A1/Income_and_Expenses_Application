26e423e54947a2b82c5a769287801b3d74035182
MBANS8A1 <seanmbng@aol.com>
2025-07-25 12:36:46 +0100

    In the object DatabaseModule, I provided the mechanism, which dagger-hilt can use for injection of the incomeDao and expenseDao into the repository implementation. With provideIncomeExpenseDatabase() method, I used the context, class name and databaseBuilder name to create a single instance of the IncomeExpenseDatabase, which will exist throughout the application's lifecycle. Next, I have used the @Provides with @Singleton to create a single instance of the incomeDao and expenseDao dependencies.

---
+++ app/src/main/java/com/example/income_and_expenses_application/di/DatabaseModule.kt
@@ -0,0 +1,43 @@
+package com.example.income_and_expenses_application.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.income_and_expenses_application.data.local.IncomeExpenseDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideIncomeExpenseDatabase(
+        @ApplicationContext context: Context
+    ): IncomeExpenseDatabase {
+        return Room.databaseBuilder(
+            context,
+            IncomeExpenseDatabase::class.java,
+            "transaction_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideExpenseDao(
+        database: IncomeExpenseDatabase
+    )= database.expenseDao
+
+
+
+    @Provides
+    @Singleton
+    fun provideIncomeDao(
+        database: IncomeExpenseDatabase
+    )= database.incomeDao
+
+
+}
