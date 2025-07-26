fa8266465b1d9fe1903d1d4d1c5201373a68225d
MBANS8A1 <seanmbng@aol.com>
2025-07-26 11:46:15 +0100

    In the HomeViewModel, I created a HomeUiState data class to track states changes in the application; I added dependency variables to data class for the list of incomes/expenses and total expenses/incomes. Next, I will want data fetching to occur with the coroutines when the application is launched, therefore I added an init{} block with a viewModelScope to allow for this. In the viewModelScope, I am using the combine function in the CoroutineScope to combine the the values emitted from the income and expense Flows; I will finish off the logic for this.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeViewModel.kt
@@ -1,16 +1,36 @@
 package com.example.income_and_expenses_application.presentation.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.income_and_expenses_application.data.local.models.Expense
 import com.example.income_and_expenses_application.data.local.models.Income
 import com.example.income_and_expenses_application.data.repository.Repository
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
     private val repository:Repository
 )
     : ViewModel(){
+    val income = repository.income
+    val expense = repository.expense
 
+    var homeUiState by mutableStateOf(HomeUiState())
+        private set
+
+    init{
+        viewModelScope.launch{
+            combine(income,expense){ incomeList: List<Income>,
+                                     expenseList: List<Expense>
+                ->
+                
+            }
+        }
+    }
 }
 
 data class HomeUiState(
