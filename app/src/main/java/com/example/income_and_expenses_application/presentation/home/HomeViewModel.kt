6a194ae757beb260263aea3be5441e7742659466
MBANS8A1 <seanmbng@aol.com>
2025-07-26 09:56:12 +0100

    To the HomeViewModel ViewModel, I added the @Inject annotation with the secondary constructor in order to inject the base Repository (to provide the single source of truth to the ViewModel). To keep track of state changes, I added the HomeUiState data class with the list of incomes/expenses and total expense/income as dependencies.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeViewModel.kt
@@ -1,2 +1,21 @@
 package com.example.income_and_expenses_application.presentation.home
 
+import androidx.lifecycle.ViewModel
+import com.example.income_and_expenses_application.data.local.models.Expense
+import com.example.income_and_expenses_application.data.local.models.Income
+import com.example.income_and_expenses_application.data.repository.Repository
+import javax.inject.Inject
+
+class HomeViewModel @Inject constructor(
+    private val repository:Repository
+)
+    : ViewModel(){
+
+}
+
+data class HomeUiState(
+    val income : List<Income> = emptyList(),
+    val expense: List<Expense> = emptyList(),
+    val totalExpense:Float = 0f,
+    val totalIncome:Float = 0f,
+)
