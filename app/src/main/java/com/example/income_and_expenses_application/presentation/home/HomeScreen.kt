3e7714329595756272fbfa29a9f7000fe5718163
MBANS8A1 <seanmbng@aol.com>
2025-07-27 10:45:08 +0100

    In the HomeScreen composable, I have completed the first item by using state and its totalIncome and totalExpense properties to calculate the balance and use it with the formatAmount() util function to format the balance as a String. This completes the first item.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeScreen.kt
@@ -9,26 +9,30 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.income_and_expenses_application.ui.theme.Income_and_Expenses_ApplicationTheme
+import com.example.income_and_expenses_application.util.formatAmount
 
 @Composable
 fun HomeScreen(
     state: HomeUiState,
     modifier: Modifier
 ) {
-
     LazyColumn(
         modifier = modifier
     ) {
         item{
             Column {
                 Row {
+                    val balance = state.totalIncome - state.totalExpense
                     Text("Your total balance:")
                     Spacer(modifier = Modifier.size(4.dp))
                     Text(
-                        text =
+                        text = "£" +
+                        formatAmount(balance),
+                        fontWeight = FontWeight.Bold
                     )
                 }
             }
