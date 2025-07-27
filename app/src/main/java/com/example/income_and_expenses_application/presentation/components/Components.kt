117a00c6185e5abc7193bf4cad775afaf797375e
MBANS8A1 <seanmbng@aol.com>
2025-07-27 18:08:34 +0100

    I have extracted the creation of the card arrows icons out into a private reusable composable function called AccountIconItem. I made a circular Surface, on which I used a Box layout to achieve the correct alignment of the image vector. I will now use the AccountIconItem within my AccountCard composable.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/components/Components.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/components/Components.kt
@@ -1,13 +1,19 @@
 package com.example.income_and_expenses_application.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import com.example.income_and_expenses_application.util.Util
@@ -38,6 +44,29 @@ fun AccountCard(
                     else Util.incomeColour.last()
             }
 
+        }
+    }
+}
+
+
+@Composable
+private fun AccountIconItem(
+    modifier: Modifier = Modifier,
+    cardIcon: ImageVector,
+    color: Color
+) {
+    Surface(
+        shape = CircleShape,
+        color =color.copy(alpha=.1f),
+        contentColor = color,
+        modifier = modifier.size(36.dp)
+    ) {
+        Box(contentAlignment = Alignment.Center){
+            Icon(
+                imageVector = cardIcon,
+                contentDescription = null,
+                modifier = Modifier.padding(4.dp)
+            )
         }
     }
 }
