ee0dbcb92695ace8f9976d6d11d5a7d1cff8450e
MBANS8A1 <seanmbng@aol.com>
2025-07-27 09:26:48 +0100

    In the HomeScreen composable, I have added a LazyColumn because the content is large, so it needs to be scrollable and load as the user scrolls. I am using an item{} scope with a nested Column and Row(nested within the Column) to add the top bar of information for the total balance. I will add another function in the Util.kt to calculate the total balance.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/home/HomeScreen.kt
@@ -1,8 +1,16 @@
 package com.example.income_and_expenses_application.presentation.home
 
+import android.widget.Space
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.income_and_expenses_application.ui.theme.Income_and_Expenses_ApplicationTheme
 
 @Composable
@@ -11,6 +19,21 @@ fun HomeScreen(
     modifier: Modifier
 ) {
 
+    LazyColumn(
+        modifier = modifier
+    ) {
+        item{
+            Column {
+                Row {
+                    Text("Your total balance:")
+                    Spacer(modifier = Modifier.size(4.dp))
+                    Text(
+                        text =
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showSystemUi = true)
