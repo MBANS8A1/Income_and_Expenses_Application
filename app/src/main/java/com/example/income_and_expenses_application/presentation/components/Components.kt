ed12fd93227543d3e1fb22c693df313c4ca0ff94
MBANS8A1 <seanmbng@aol.com>
2025-07-27 15:13:28 +0100

    Next, in the Components.kt file, I created an AccountCard composable with a modifier, cardTitle, amount and cardIcon as parameters. This composable will render the cards for "Total Income" and "Total Expense". To the body of the composable function, I added a Card with a Column nested inside that is centered vertically and horizontally with 10.dp of spacing. As the cardIcon is a nullable ImageVector (as the card should be generated even without an icon), I am checking if it is not null; if this is true, I will set the icon's attributes.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/components/Components.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/components/Components.kt
@@ -1,2 +1,41 @@
 package com.example.income_and_expenses_application.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AccountCard(
+    modifier: Modifier = Modifier,
+    cardTitle: String,
+    amount:String,
+    cardIcon: ImageVector?
+) {
+
+    Card(
+        modifier = Modifier
+    ){
+        Column(
+            verticalArrangement = Arrangement.spacedBy(
+                space=10.dp,
+                alignment = Alignment.CenterVertically
+                ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            if(cardIcon != null){
+                val iconColor = if(cardTitle)
+            }
+
+        }
+    }
+}
