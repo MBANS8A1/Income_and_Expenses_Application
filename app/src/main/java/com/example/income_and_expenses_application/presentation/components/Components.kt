e2f0b4fb0765fcfd73f357ff8a24fef5d13ca041
MBANS8A1 <seanmbng@aol.com>
2025-07-29 21:21:51 +0100

    I have added some UI semantics to the SeeAllButton function called on line 144 in the Components.kt file. I added a contentDescription (within a clearAndSetSemantics() scope), which can be "All Income" or "All Expense". This can be helpful to people with disabilities.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/components/Components.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/components/Components.kt
@@ -24,6 +24,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -140,7 +142,9 @@ private fun <T> OverViewCard(
                     row(it)
                 }
                 SeeAllButton(
-                    modifier = Modifier,
+                    modifier = Modifier.clearAndSetSemantics {
+                        contentDescription = "All $title"
+                    },
                     onClickSeeAll = onClickSeeAll
                 )
             }
