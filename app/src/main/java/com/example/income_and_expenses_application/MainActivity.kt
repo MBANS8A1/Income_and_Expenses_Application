b63be400bd26772156e46ca6732ad16a0642954e
MBANS8A1 <seanmbng@aol.com>
2025-07-31 18:56:14 +0100

    I converted the Scaffold in the MainActivity.kt file to a Surface instead. Next, I created a variable in the setContent scope to reference the HomeViewModel (ViewModel) I made earlier. I have initialized the arguments for the HomeScreen() composable, which is called in the Surface scope. But, the click listeners will need full implementation.

--- app/src/main/java/com/example/income_and_expenses_application/MainActivity.kt
+++ app/src/main/java/com/example/income_and_expenses_application/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.income_and_expenses_application.presentation.home.HomeScreen
+import com.example.income_and_expenses_application.presentation.home.HomeViewModel
 import com.example.income_and_expenses_application.ui.theme.Income_and_Expenses_ApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +23,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val homeViewModel: HomeViewModel = viewModel()
             Income_and_Expenses_ApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color= MaterialTheme.colorScheme.background
+                ){
+                    HomeScreen(
+                        state = homeViewModel.homeUiState,
+                        modifier = Modifier,
+                        onClickSeeAllExpense = {},
+                        onClickSeeAllIncome = {},
+                        onExpenseClick = {},
+                        onIncomeClick = {},
+                        onInsertIncome = homeViewModel::insertIncome,
+                        onInsertExpense = homeViewModel::insertExpense
+
                     )
                 }
             }
