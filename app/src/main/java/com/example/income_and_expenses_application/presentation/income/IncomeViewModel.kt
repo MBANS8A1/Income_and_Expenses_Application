37e2e8a71755c7d6271f57710ce3f8dca46a7762
MBANS8A1 <seanmbng@aol.com>
2025-08-01 16:47:30 +0100

    I created a getAllIncome() function and initialised it to a CoroutineScope by using a viewModelScope. By accessing the repository income (Flow<List<Income>>) and collecting the emitted value (List<Income>), I have made a copy of the IncomeState observable where I can keep the list of Incomes.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/income/IncomeViewModel.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/income/IncomeViewModel.kt
@@ -1,17 +1,32 @@
 package com.example.income_and_expenses_application.presentation.income
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.income_and_expenses_application.data.local.models.Income
 import com.example.income_and_expenses_application.data.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class IncomeViewModel @Inject constructor(
     private val repository: Repository
 ):ViewModel() {
-}
+    var incomeState by mutableStateOf(IncomeState())
+        private set
 
+    fun getAllIncome() = viewModelScope.launch {
+        repository.income.collectLatest {
+            incomeState.copy(
+                incomes = it
+            )
+        }
+    }
+}
 
 data class IncomeState(
     val incomes: List<Income> = emptyList()
