82e9cc1cf78441c67219bd340c62113cfaa83254
MBANS8A1 <seanmbng@aol.com>
2025-08-01 14:57:15 +0100

    I have started the process of making the Income screen of the application. I made an income package in the presentation layer (folder).  Within this subdirectory, I made a ViewModel class called IncomeViewModel, which will pass the needed data to the future Income screen. I have also created a data class for managing Income state.

---
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/income/IncomeViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.income_and_expenses_application.presentation.income
+
+import androidx.lifecycle.ViewModel
+import com.example.income_and_expenses_application.data.local.models.Income
+import com.example.income_and_expenses_application.data.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class IncomeViewModel @Inject constructor(
+    private val repository: Repository
+):ViewModel() {
+}
+
+
+data class IncomeState(
+    val income: List<Income> = emptyList()
+)
