cbaf698387b991aa6c99b02ad86be744cbfaa6f2
MBANS8A1 <seanmbng@aol.com>
2025-08-02 19:41:46 +0100

    As there can be an almost unlimited amount of Income items, I added a LazyColumn layout within the TransitionStatement composable. In the LazyColumn, I added an item{} block, which should contain a Box layout for the animated circle/ring. The animated circle has be externalized to a composable called AnimateCircle where the dimensions, colours and properties will be managed (especially the state of transition animations with MutableTransitionState).

--- app/src/main/java/com/example/income_and_expenses_application/presentation/components/TransactionStatement.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/components/TransactionStatement.kt
@@ -1,8 +1,14 @@
 package com.example.income_and_expenses_application.presentation.components
 
 import android.graphics.Color
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun <T> TransactionStatement(
@@ -16,5 +22,26 @@ fun <T> TransactionStatement(
     key: (T) -> Int,
     rows:@Composable (T) -> Unit
 ) {
-    
+    LazyColumn(
+        modifier = modifier
+    ) {
+        item{
+            Box(modifier = Modifier.padding(16.dp)){
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun AnimateCircle(
+    proportions: List<Float>,
+    colours: List<Color>,
+    modifier: Modifier = Modifier
+
+) {
+    val currentState = remember{
+        MutableTransitionState
+    }
 }
