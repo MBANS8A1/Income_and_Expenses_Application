39944d2bfef26f480f9a078c7a3762b0f9d22028
MBANS8A1 <seanmbng@aol.com>
2025-08-02 23:59:06 +0100

    I created a custom angle offset for the circular ring by mapping to the targetValue animation (in degrees) by using an animate.Float; also, I used a tween transitionSpec (with a duration of 900 ms, a delay of 500ms and a LinearOutSlowInEasing animation function) to control the start and end of the transition. The targetValueByState attribute of animateFloat is controlled by a conditional if-else statement.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/components/TransactionStatement.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/components/TransactionStatement.kt
@@ -1,13 +1,17 @@
 package com.example.income_and_expenses_application.presentation.components
 
 import android.graphics.Color
+import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.rememberTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -54,6 +58,21 @@ fun AnimateCircle(
         Stroke(5.dp.toPx())
     }
     val transition = rememberTransition(transitionState = currentState, label = "Circle/Ring")
+    //angle offset for offsetting the cycle
+    val angleOffset by transition.animateFloat(
+        transitionSpec ={ tween(
+            durationMillis = 900,
+            delayMillis = 500,
+            easing = LinearOutSlowInEasing
+        )},
+        label = ""
+    ){progress ->
+        if(progress == AnimatedCircleProgress.START){
+            0f
+        }else{
+            360f
+        }
+    }
 }
 
 private enum class AnimatedCircleProgress{
