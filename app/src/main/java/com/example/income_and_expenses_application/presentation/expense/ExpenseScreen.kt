7a4304940a1a5b24ea6aa4d9516758ca0f04688c
MBANS8A1 <seanmbng@aol.com>
2025-08-05 15:57:06 +0100

    I have recycled the code from the IncomeScreen.kt file to the ExpenseScreen.kt file and just replaced the name of the composables, used different imports and changed the variable names.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/expense/ExpenseScreen.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/expense/ExpenseScreen.kt
@@ -1,2 +1,73 @@
 package com.example.income_and_expenses_application.presentation.expense
 
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.income_and_expenses_application.data.local.models.Expense
+import com.example.income_and_expenses_application.presentation.components.ExpenseRow
+import com.example.income_and_expenses_application.presentation.components.IncomeRow
+import com.example.income_and_expenses_application.presentation.components.TransactionStatement
+import com.example.income_and_expenses_application.presentation.income.IncomeScreen
+import com.example.income_and_expenses_application.util.Util
+import com.example.income_and_expenses_application.util.expenseList
+import com.example.income_and_expenses_application.util.getColour
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+@Composable
+fun ExpenseScreen(
+    modifier: Modifier = Modifier,
+    expenses: List<Expense>,
+    onExpenseItemClick: (id:Int) -> Unit,
+    onExpenseItemDelete: (Int) -> Unit
+) {
+    TransactionStatement(
+        modifier = modifier,
+        items = expenses,
+        colours = { getColour(it.expenseAmount.toFloat(),
+            Util.expenseColour
+        ) },
+        amounts = {it.expenseAmount.toFloat()},
+        amountsTotal = expenses.sumOf { it.expenseAmount }.toFloat(),
+        circleLabel = "Pay",
+        onItemSwiped = {
+            onExpenseItemDelete.invoke(it.id)
+        },
+        key = {it.id},  //will increase performance of the composable
+
+    ){
+        //row composable scope
+        ExpenseRow(
+            name = it.title,
+            description = "Receive ${formatDetailDate(it.date)}",
+            amount= it.expenseAmount.toFloat(),
+            colour = getColour(it.expenseAmount.toFloat(),
+                Util.expenseColour
+            ),
+            modifier = Modifier.clickable {
+                onExpenseItemClick.invoke(it.id)
+            }
+
+        )
+    }
+}
+
+//A function to format the Date
+fun formatDetailDate(date: Date):String =
+    SimpleDateFormat("dd MM", Locale.getDefault()).format(date)
+
+
+
+@Preview(showSystemUi = true)
+@Composable
+private fun PrevExpenseScreen() {
+    ExpenseScreen(
+        expenses = expenseList.mapIndexed{ index, expense ->
+            expense.copy(id = index)
+        },
+        onExpenseItemClick = {},
+        onExpenseItemDelete = {}
+    )
+}
