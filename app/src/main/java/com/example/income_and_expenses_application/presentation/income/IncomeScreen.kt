f5c70f567f7cd7b9dcfac3f19da4425dcb87c972
MBANS8A1 <seanmbng@aol.com>
2025-08-05 13:26:22 +0100

    To make the rows of Income items clickable, I added the modifier argument to the IncomeRow() and invoked onIncomeItemClick() within the clickable lambda. Also, I added a preview composable called PrevIncomeScreen(), which makes use of the incomeList data in the Util.kt file. But, as the ids of the Incomes are all the same, I am mapping the ids of Incomes in the incomeList to their index position using the Collection function mapIndexed{}.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/income/IncomeScreen.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/income/IncomeScreen.kt
@@ -1,12 +1,15 @@
 package com.example.income_and_expenses_application.presentation.income
 
+import androidx.compose.foundation.clickable
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.income_and_expenses_application.data.local.models.Income
 import com.example.income_and_expenses_application.presentation.components.IncomeRow
 import com.example.income_and_expenses_application.presentation.components.TransactionStatement
 import com.example.income_and_expenses_application.util.Util
 import com.example.income_and_expenses_application.util.getColour
+import com.example.income_and_expenses_application.util.incomeList
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -40,7 +43,11 @@ fun IncomeScreen(
                 amount= it.incomeAmount.toFloat(),
                 colour = getColour(it.incomeAmount.toFloat(),
                     Util.incomeColour
-                )
+                ),
+            modifier = Modifier.clickable {
+                onIncomeItemClick.invoke(it.id)
+            }
+
         )
     }
 }
@@ -48,3 +55,16 @@ fun IncomeScreen(
 //A function to format the Date
 fun formatDetailDate(date: Date):String =
     SimpleDateFormat("dd MM", Locale.getDefault()).format(date)
+
+
+@Preview(showSystemUi = true)
+@Composable
+private fun PrevIncomeScreen() {
+    IncomeScreen(
+        incomes = incomeList.mapIndexed{ index, income ->
+            income.copy(id = index)
+        },
+        onIncomeItemClick = {},
+        onIncomeItemDelete = {}
+    )
+}
