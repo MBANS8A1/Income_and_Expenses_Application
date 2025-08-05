ec2024819835182f20d7c0ae1b418cebb04b5f37
MBANS8A1 <seanmbng@aol.com>
2025-08-05 18:20:58 +0100

    I want to manage the navigation between the screens and activities, so I have created a new navigation package with a Navigation.kt Kotlin file inside; I will create my IncomeExpenseNavigation composable in this file.

---
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/navigation/Navigation.kt
@@ -0,0 +1,9 @@
+package com.example.income_and_expenses_application.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun IncomeExpenseNavigation(modifier: Modifier = Modifier) {
+    
+}
