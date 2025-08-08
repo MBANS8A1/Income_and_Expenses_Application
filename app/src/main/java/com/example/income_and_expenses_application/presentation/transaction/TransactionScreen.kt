dd718d206cb21bf3c210cb6ff798f4b152681945
MBANS8A1 <seanmbng@aol.com>
2025-08-08 15:32:32 +0100

    The next component for the TransactionScreen is the DatePicker. I created an external composable function called IncExpDate(). Inside is the datePickerState variable, which uses the rememberDatePickerState to keep track of the DatePicker when recomposition occurs. I also made a state observable called openDateDialog to decide when to show the DatePickerDialog. Below is a Row layout with a Calendar icon, the current date as a a String and the DatePickerDialog, which is created when openDateDialog is true. But, when a date is picked openDateDialog becomes false and there is no way for openDateDialog to become true again, so I need add the logic for this still.

--- app/src/main/java/com/example/income_and_expenses_application/presentation/transaction/TransactionScreen.kt
+++ app/src/main/java/com/example/income_and_expenses_application/presentation/transaction/TransactionScreen.kt
@@ -1,19 +1,36 @@
 package com.example.income_and_expenses_application.presentation.transaction
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import com.example.income_and_expenses_application.util.formatDate
+import java.util.Date
 
 @Composable
 fun TransactionScreen(
@@ -35,7 +52,63 @@ fun TransactionScreen(
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
         )
         Spacer(modifier = Modifier.size(12.dp))
+        //Add the date picker
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun IncExpDate(
+    date: Date,
+    onDateChange: (Long?) -> Unit,
+
+) {
+
+    val datePickerState =  rememberDatePickerState() //still experimental
+    //tracking opening of the dialog
+    var openDateDialog by remember { mutableStateOf(value=false) }
+    //Now to add the data arranged in a row format
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Icon(
+            imageVector = Icons.Default.DateRange,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(text = formatDate(date))
+        Spacer(modifier = Modifier.size(4.dp))
+        if(openDateDialog){
+            DatePickerDialog(
+                onDismissRequest = {openDateDialog = false},
+                confirmButton = {
+                    Button(
+                        onClick = {
+                            onDateChange(datePickerState.selectedDateMillis)
+                        }
+                    ){
+                        Text(text="Submit")
+
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            openDateDialog = false
+                        }
+                    ) {
+                        Text(text="Dismiss")
+                    }
+                },
+                content = {
+                    DatePicker(
+                        state = datePickerState,
+                    )
+                }
+            )
+        }
+        //need to add an icon to be able to convert openDateDialog back to to true
     }
 }
 
